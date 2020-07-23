476d4863825699da6da220a7ccfda92fd2a7827d
andrey678 <drew6496@yandex.ru>
2020-07-23 11:18:33 +0300

    Update logger to include self.instance

--- app.rb
+++ app.rb
@@ -3,18 +3,33 @@ class Logger
   def initialize
     @f = File.open 'log.txt','a'
   end
-  
+
+@@x = nil
+
+  def self.instance
+
+         if @@x == nil
+            @@x = Logger.new
+          end
+
+  return @@x  
+
+  end
+
+  #CLASS METHOD
   def self.say_something
     puts "haha"
   end
 
-
+#Instance method
   def log_something wat
   @f.puts wat
   end
 end
 
 Logger.say_something
+Logger.instance.log_something 'blabla'
+Logger.instance.log_something 'blabla2'
 
 logger = Logger.new
 logger.log_something 'hey!'
